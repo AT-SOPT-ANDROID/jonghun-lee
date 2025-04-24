c734952f9f7a3ebdb9745d589b3ef33c585fd02c
fredLeeJH <fred54755475@gmail.com>
2025-04-25 01:45:40 +0900

    feat: add HistoryScreen

---
+++ app/src/main/java/org/sopt/at/presentation/history/HistoryScreen.kt
@@ -0,0 +1,22 @@
+package org.sopt.at.presentation.history
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun HistoryScreen(){
+    Column(modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center) {
+        Text(
+            text = "history Screen",
+            color = Color.White
+        )
+    }
+}
