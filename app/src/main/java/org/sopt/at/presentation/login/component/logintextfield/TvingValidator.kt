a767e0bbbc87b349d8bc4867f5d085ab92776157
fredLeeJH <fred54755475@gmail.com>
2025-05-01 22:13:59 +0900

    [refactor] week3

--- app/src/main/java/org/sopt/at/presentation/login/component/logintextfield/TvingValidator.kt
+++ app/src/main/java/org/sopt/at/presentation/login/component/logintextfield/TvingValidator.kt
@@ -1,4 +1,4 @@
-package org.sopt.at.login.component.logintextfield
+package org.sopt.at.presentation.login.component.logintextfield
 
 object TvingValidator {
     val idPattern = Regex("^[a-z0-9]{6,12}$")
