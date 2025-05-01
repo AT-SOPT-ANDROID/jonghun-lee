1757d93e15dfca9c16227206a22601754d80ac6c
fredLeeJH <fred54755475@gmail.com>
2025-05-01 22:12:47 +0900

    [refactor] ShortsScreen 변경

--- app/src/main/java/org/sopt/at/presentation/shorts/ShortsScreen.kt
+++ app/src/main/java/org/sopt/at/presentation/shorts/ShortsScreen.kt
@@ -1,5 +1,6 @@
 package org.sopt.at.presentation.shorts
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,10 +11,22 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 
 @Composable
-fun ShortsScreen() {
-    Column(modifier = Modifier.fillMaxSize(),
+fun ShortsRoute(modifier: Modifier = Modifier) {
+    ShortsScreen(modifier = modifier)
+
+}
+
+@Composable
+private fun ShortsScreen(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center) {
+        verticalArrangement = Arrangement.Center
+    ) {
         Text(
             text = "shorts Screen",
             color = Color.White
