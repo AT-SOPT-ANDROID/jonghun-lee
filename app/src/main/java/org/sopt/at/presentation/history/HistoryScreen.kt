a4a59d83344d4b3c71bc55d59c292d6e65b730b5
fredLeeJH <fred54755475@gmail.com>
2025-05-01 22:12:08 +0900

    [refactor] HistoryScreen 변경

--- app/src/main/java/org/sopt/at/presentation/history/HistoryScreen.kt
+++ app/src/main/java/org/sopt/at/presentation/history/HistoryScreen.kt
@@ -1,5 +1,6 @@
 package org.sopt.at.presentation.history
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,10 +11,19 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 
 @Composable
-fun HistoryScreen(){
-    Column(modifier = Modifier.fillMaxSize(),
+fun HistoryRoute(modifier: Modifier = Modifier) {
+    HistoryScreen(modifier = modifier)
+}
+
+@Composable
+fun HistoryScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center) {
+        verticalArrangement = Arrangement.Center
+    ) {
         Text(
             text = "history Screen",
             color = Color.White
