2f003353a3a9d744af187a5332ef9457a1aa3110
fredLeeJH <fred54755475@gmail.com>
2025-05-02 16:00:21 +0900

    [refactor] sticky header

---
+++ app/src/main/java/org/sopt/at/presentation/home/data/TabItem.kt
@@ -0,0 +1,11 @@
+package org.sopt.at.presentation.home.data
+
+enum class TabItem(val title: String) {
+    MAIN("전체"),
+    DRAMA("드라마"),
+    ENTERTAINMENT("예능"),
+    MOVIE("영화"),
+    SPORTS("스포츠"),
+    ANNIE("애니"),
+    NEWS("뉴스")
+}
