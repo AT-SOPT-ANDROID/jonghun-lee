71f4739e7f8c31b47af5590bafefc6711d319872
fredLeeJH <fred54755475@gmail.com>
2025-05-07 16:20:08 +0900

    [refactor] BottomNavigationBar 불변 처리

--- app/src/main/java/org/sopt/at/main/component/BottomNavigationBar.kt
+++ app/src/main/java/org/sopt/at/main/component/BottomNavigationBar.kt
@@ -2,14 +2,16 @@ package org.sopt.at.main.component
 
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.key
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import kotlinx.collections.immutable.ImmutableList
 import org.sopt.at.main.MainTab
 
 @Composable
 fun BottomNavigationBar(
-    tabs: List<MainTab>,
+    tabs: ImmutableList<MainTab>,
     currentTab: MainTab?,
     onTabSelected: (MainTab) -> Unit,
     modifier: Modifier = Modifier
@@ -19,27 +21,29 @@ fun BottomNavigationBar(
         containerColor = Color.Black
     ) {
         tabs.forEach { tab ->
-            val selected = currentTab == tab
-            NavigationBarItem(
-                selected = selected,
-                onClick = { onTabSelected(tab) },
-                icon = {
-                    Icon(
-                        painter = painterResource(id = tab.icon),
-                        contentDescription = tab.contentDescription,
-                        tint = if (selected) Color.White else Color(0xFF9A9A9A)
+            key(tab.route) {
+                val selected = currentTab == tab
+                NavigationBarItem(
+                    selected = selected,
+                    onClick = { onTabSelected(tab) },
+                    icon = {
+                        Icon(
+                            painter = painterResource(id = tab.icon),
+                            contentDescription = tab.contentDescription,
+                            tint = if (selected) Color.White else Color(0xFF9A9A9A)
+                        )
+                    },
+                    label = {
+                        Text(
+                            text = tab.contentDescription,
+                            color = if (selected) Color.White else Color(0xFF9A9A9A)
+                        )
+                    },
+                    colors = NavigationBarItemDefaults.colors(
+                        indicatorColor = Color.Transparent
                     )
-                },
-                label = {
-                    Text(
-                        text = tab.contentDescription,
-                        color = if (selected) Color.White else Color(0xFF9A9A9A)
-                    )
-                },
-                colors = NavigationBarItemDefaults.colors(
-                    indicatorColor = Color.Transparent
                 )
-            )
+            }
         }
     }
 }
